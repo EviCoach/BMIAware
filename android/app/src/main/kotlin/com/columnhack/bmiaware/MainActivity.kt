960f19b9b82de8ffca81586e21aa953d84f12537
evicoach@gmail.com <evicoach@gmail.com>
2020-03-30 18:41:37 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/com/columnhack/bmiaware/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.columnhack.bmiaware
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BMI Aware",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Aware"),
+      ),
+    );
+  }
+}
